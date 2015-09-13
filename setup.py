5d9670949c96bf2a9ce804d69b3166dac0303aba
Robert Weidlich <robert@rom.localdomain>
2015-09-13 20:14:39 +0200

    Changed Version to git-based one

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ import os
 
 setup(name='test-pkg',
       version="1.0",
+      version_format="1.0.dev{commitcount}+{gitsha}"
+      setup_requires=['setuptools-git-version'],
       description='Application for testing CI',
       long_description="",
       author='Robert Weidlich',
